647413c8865e98ecc140bc5168be0c8f2a13089a
Pratik Solanki <pratiks.solanki3@gmail.com>
2020-02-21 19:04:50 +0530

    Adding Stop words

--- app/Http/Controllers/WikiController.php
+++ app/Http/Controllers/WikiController.php
@@ -34,9 +34,11 @@ class WikiController extends Controller
         $count = count($sentence_arr);
         if($count>0){
             $final = array();
+            $stop_words = "i,me,my,myself,we,our,ours,ourselves,you,your,yours,yourself,yourselves,he,him,his,himself,she,her,hers,herself,it,its,itself,they,them,their,theirs,themselves,what,which,who,whom,this,that,these,those,am,is,are,was,were,be,been,being,have,has,had,having,do,does,did,doing,a,an,the,and,but,if,or,because,as,until,while,of,at,by,for,with,about,against,between,into,through,during,before,after,above,below,to,from,up,down,in,out,on,off,over,under,again,further,then,once,here,there,when,where,why,how,all,any,both,each,few,more,most,other,some,such,no,nor,not,only,own,same,so,than,too,very,s,t,can,will,just,don,should,now";
+            $stop_words = explode(',', $stop_words);
             for($i =0 ;$i<$count;$i++){
-                if(ctype_alnum($sentence_arr[$i])){
-                    $sentence_arr[$i] = strtolower($sentence_arr[$i]);
+                $sentence_arr[$i] = strtolower($sentence_arr[$i]);
+                if(ctype_alnum($sentence_arr[$i]) && !in_array($sentence_arr[$i],$stop_words)){
                     if($final[$sentence_arr[$i]]){
                         $final[$sentence_arr[$i]] += 1;
                     } else {
